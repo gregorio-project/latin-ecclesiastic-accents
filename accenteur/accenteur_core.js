21c5fc6ea1a7d865f187009998e4449644f3a57e
BrRoman <editeur@traditions-monastiques.com>
2018-09-24 19:12:08 +0200

    'gu' OK.

--- accenteur/accenteur_core.js
+++ accenteur/accenteur_core.js
@@ -157,7 +157,7 @@ function qty_to_accent(plain, quantified){
                     quantities[i] = "-";
                 }
             }
-            if((["e", "u"].indexOf(plain[i]) != -1 && ["a", "e", "A", "E", "q"].indexOf(plain[i - 1]) != -1) == false){ // If c is not the second letter of "au", "eu", "ae", "oe", "qu".
+            if((["e", "u"].indexOf(plain[i]) != -1 && ["a", "e", "A", "E", "q", "g"].indexOf(plain[i - 1]) != -1) == false){ // If c is not the second letter of "au", "eu", "ae", "oe", "qu", "gu".
                 num_syllables ++;
             }
         }
